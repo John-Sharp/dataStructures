4b3a0e22703074125a95befb55df9f6f07cd2ce5
John Sharp <johncolinsharp@gmail.com>
2020-04-12 14:47:50 +0100

    added addToEnd function template

--- templates/listTemplate.h
+++ templates/listTemplate.h
@@ -12,6 +12,9 @@ bool <pf>Cmp(const <tp> * a, const <tp> * b);
 // <pf>ListAdd: adds element to head of list
 <pf>List * <pf>ListAdd(<pf>List * l, <tp> * val);
 
+// <pf>ListAdd: adds element to end of list
+<pf>List * <pf>ListAddToEnd(<pf>List * l, <tp> * val);
+
 // <pf>ListRmremove: removes element from list
 <pf>List * <pf>ListRm(<pf>List * l, const <tp> * val, bool (*cmp)(const <tp> * a, const <tp> * b), <tp> ** removedVal);
 
